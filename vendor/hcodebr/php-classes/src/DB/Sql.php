7015bc9aa15b39963c2ec5fe74c725cf8685519d
Lazaro <renatolazaroch@gmail.com>
2020-01-07 11:00:43 -0300

    :construction: [Chore] Adicionado TPL site

--- vendor/hcodebr/php-classes/src/DB/Sql.php
+++ vendor/hcodebr/php-classes/src/DB/Sql.php
@@ -1,94 +1,69 @@
-<?php
-
-declare(strict_types=1);
+<?php 
 
 namespace Hcode\DB;
 
-class Sql
-{
-    public const HOSTNAME = '127.0.0.1';
-    public const USERNAME = 'root';
-    public const PASSWORD = '';
-    public const DBNAME = 'db_ecommerce';
+class Sql {
 
-    private $conn;
+	const HOSTNAME = "127.0.0.1";
+	const USERNAME = "root";
+	const PASSWORD = "root";
+	const DBNAME = "db_ecommerce";
 
-    /**
-     * __construct.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        $this->conn = new \PDO(
-            'mysql:dbname='.self::DBNAME.';host='.self::HOSTNAME,
-            self::USERNAME,
-            self::PASSWORD
-        );
-    }
+	private $conn;
 
-    /**
-     * query.
-     *
-     * @param mixed $rawQuery
-     * @param mixed $params
-     *
-     * @return void
-     */
-    public function query($rawQuery, $params = [])
-    {
-        $stmt = $this->conn->prepare($rawQuery);
+	public function __construct()
+	{
 
-        $this->setParams($stmt, $params);
+		$this->conn = new \PDO(
+			"mysql:dbname=".Sql::DBNAME.";host=".Sql::HOSTNAME, 
+			Sql::USERNAME,
+			Sql::PASSWORD
+		);
 
-        $stmt->execute();
-    }
+	}
 
-    /**
-     * select.
-     *
-     * @param mixed $rawQuery
-     * @param mixed $params
-     *
-     * @return array
-     */
-    public function select($rawQuery, $params = []): array
-    {
-        $stmt = $this->conn->prepare($rawQuery);
+	private function setParams($statement, $parameters = array())
+	{
 
-        $this->setParams($stmt, $params);
+		foreach ($parameters as $key => $value) {
+			
+			$this->bindParam($statement, $key, $value);
 
-        $stmt->execute();
+		}
 
-        return $stmt->fetchAll(\PDO::FETCH_ASSOC);
-    }
+	}
 
-    /**
-     * setParams.
-     *
-     * @param mixed $statement
-     * @param mixed $parameters
-     *
-     * @return void
-     */
-    private function setParams($statement, $parameters = [])
-    {
-        foreach ($parameters as $key => $value) {
-            $this->bindParam($statement, $key, $value);
-        }
-    }
+	private function bindParam($statement, $key, $value)
+	{
 
-    /**
-     * bindParam.
-     *
-     * @param mixed $statement
-     * @param mixed $key
-     * @param mixed $value
-     *
-     * @return void
-     */
-    private function bindParam($statement, $key, $value)
-    {
-        $statement->bindParam($key, $value);
-    }
+		$statement->bindParam($key, $value);
+
+	}
+
+	public function query($rawQuery, $params = array())
+	{
+
+		$stmt = $this->conn->prepare($rawQuery);
+
+		$this->setParams($stmt, $params);
+
+		$stmt->execute();
+
+	}
+
+	public function select($rawQuery, $params = array()):array
+	{
+
+		$stmt = $this->conn->prepare($rawQuery);
+
+		$this->setParams($stmt, $params);
+
+		$stmt->execute();
+
+		return $stmt->fetchAll(\PDO::FETCH_ASSOC);
+
+	}
+
 }
+
+ ?>
