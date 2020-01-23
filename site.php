d0dcf9f56bc27f32e0c6d862d4c7a417af5060d3
Lazaro <renatolazaroch@gmail.com>
2020-01-23 15:26:58 -0300

    :construction: [Chore] Criado paginação

--- site.php
+++ site.php
@@ -14,13 +14,23 @@ $app->get('/', function () {
     ]);
 });
 
-// Categoria da loja front
+// Rota Paginação
 $app->get('/categories/:idcategory', function ($idcategory) {
+    $page = (isset($_GET['page'])) ? (int) $_GET['page'] : 1;
     $category = new Category();
     $category->getCategory((int) $idcategory);
+    $pagination = $category->getProductsPage($page);
+    $pages = [];
+    for ($i = 1; $i <= $pagination['pages']; ++$i) {
+        array_push($pages, [
+            'link' => '/categories/'.$category->getidcategory().'?page='.$i,
+            'page' => $i,
+        ]);
+    }
     $page = new Page();
     $page->setTpl('category', [
-         'category' => $category->getValues(),
-         'products' => Product::checkList($category->getProducts()),
-     ]);
+        'category' => $category->getValues(),
+        'products' => $pagination['data'],
+        'pages' => $pages,
+    ]);
 });
